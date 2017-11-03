59555f0953cadf461a0fce3d944dd84b6b7c7ea0
mortenlaursen <mortendlaursen@gmail.com>
2017-11-03 10:27:42 +0100

    opening hours are added. May need to be modified to show peak hours for a certain day instead.

--- components/MapPage.js
+++ components/MapPage.js
@@ -1,14 +1,13 @@
 import React, {Component} from 'react';
 import ReactNative from 'react-native';
-import { Icon } from 'react-native-elements';
 import MapView from 'react-native-maps';
 const {
     View,
+    Dimensions,
+    Modal,
     Text,
-    Dimensions
+    TouchableHighlight
 } = ReactNative;
-import ActionButton from './ActionButton';
-import * as firebase from 'firebase';
 
 class MapPage extends Component {
     constructor() {
@@ -22,7 +21,44 @@ class MapPage extends Component {
                     coordinates: {
                         latitude: 55.7000354,
                         longitude: 12.57803100000001
-                    }
+                    },
+                    openingHours: [
+                        {
+                            day: 'M',
+                            hours: '10 - 19',
+                            value: 30,
+                        },
+                        {
+                            day: 'T',
+                            hours: '10 - 19',
+                            value: 40,
+                        },
+                        {
+                            day: 'O',
+                            hours: '10 - 19',
+                            value: 50,
+                        },
+                        {
+                            day: 'T',
+                            hours: '10 - 19',
+                            value: 60,
+                        },
+                        {
+                            day: 'F',
+                            hours: '10 - 19',
+                            value: 60,
+                        },
+                        {
+                            day: 'L',
+                            hours: '10 - 19',
+                            value: 20,
+                        },
+                        {
+                            day: 'S',
+                            hours: '10 - 19',
+                            value: 30,
+                        }
+                    ]
                 },
                 {
                     type: 'farmacia',
@@ -31,9 +67,47 @@ class MapPage extends Component {
                     coordinates: {
                         latitude: 55.7094258,
                         longitude: 12.577164799999991
-                    }
+                    },
+                    openingHours: [
+                        {
+                            day: 'M',
+                            hours: '10 - 19',
+                            value: 30,
+                        },
+                        {
+                            day: 'T',
+                            hours: '10 - 19',
+                            value: 40,
+                        },
+                        {
+                            day: 'O',
+                            hours: '10 - 19',
+                            value: 50,
+                        },
+                        {
+                            day: 'T',
+                            hours: '10 - 19',
+                            value: 60,
+                        },
+                        {
+                            day: 'F',
+                            hours: '10 - 19',
+                            value: 60,
+                        },
+                        {
+                            day: 'L',
+                            hours: '10 - 19',
+                            value: 20,
+                        },
+                        {
+                            day: 'S',
+                            hours: '10 - 19',
+                            value: 30,
+                        }
+                    ]
                 }
-            ]
+            ],
+            modalVisible: false,
             /*region: {
                 latitude: 37.78825,
                 longitude: -122.4324,
@@ -72,6 +146,25 @@ class MapPage extends Component {
     render() {
         return (
             <View style = {styles.preview}>
+                {/*<Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {alert("Modal has been closed.")}}
+                >
+                    <View style={{marginTop: 22}}>
+                        <View>
+                            <Text>Hello World!</Text>
+
+                            <TouchableHighlight onPress={() => {
+                                this.setState({modalVisible:false})
+                            }}>
+                                <Text>Hide Modal</Text>
+                            </TouchableHighlight>
+
+                        </View>
+                    </View>
+                </Modal>*/}
                 <MapView
                     //minZoomLevel={10}
                     style = {styles.map}
@@ -79,31 +172,53 @@ class MapPage extends Component {
                     //Region={this.state.region}
                     onRegionChange={this.onRegionChange.bind(this)}
                 >
-                    {this.state.pharmacies.map((marker) => {
+                    {this.state.pharmacies.map((pharmacy) => {
                         return(
                             <MapView.Marker
-                                coordinate={marker.coordinates}
-                                title={marker.title}
-                                description={marker.description}
-                            />
+                                style={{padding:20}}
+                                coordinate={pharmacy.coordinates}
+                                //title={pharmacy.title}
+                                //description={pharmacy.description}
+                                //onPress={() => this.setState({modalVisible:true})}
+                                //popover={PopoverElement}
+                                //onPress={popover.showPopper.bind(this)}
+                            >
+                                <MapView.Callout>
+                                    <View
+                                        style={{
+                                        }}
+                                    >
+                                        <Text
+                                            style={{fontSize:20, textAlign: 'center' }}
+                                            >
+                                            {pharmacy.title}
+                                        </Text>
+                                        <Text style={{color: 'grey'}}
+                                            >
+                                            {pharmacy.description}
+                                        </Text>
+                                        <View
+                                            style={styles.openingHoursContainer}
+                                            >
+                                            {pharmacy.openingHours.map((day) => {
+                                                return(
+                                                    <View style={styles.openingHours}>
+                                                        <View style={[styles.hoursColumn, {height: day['value']} ]}/>
+                                                        <Text style={{textAlign:'center'}}>{day.day}</Text>
+                                                    </View>
+
+                                                )
+                                            })}
+
+                                        </View>
+                                    </View>
+                                </MapView.Callout>
+                            </MapView.Marker>
                         )
                     })}
                 </MapView>
             </View>
         )
-            /*
-            <View>
-                <Text>
-                    CurrentPosition: {this.state.currentPosition.lat}, {this.state.currentPosition.long}
-                </Text>
-                <ActionButton
-                    title='getLocation'
-                    onPress={this.getCurrentLocation.bind(this)}
-                    >
-
-                </ActionButton>
-            </View>
-        )*/
     }
 }
 
@@ -121,7 +236,33 @@ const styles = {
         left: 0,
         right: 0,
         bottom: 0
+    },
+    openingHoursContainer: {
+        position: 'relative',
+        width:'100%',
+        display:'flex',
+        flexDirection:'row',
+        height:100,
+        alignItems: 'baseline',
+        marginTop: 20,
+    },
+    openingHours: {
+        flex:1,
+        marginLeft: 'auto',
+        marginRight: 'auto'
+    },
+    hoursColumn: {
+        width:20,
+        backgroundColor: '#7ba9f7',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        borderTopLeftRadius: 3,
+        borderTopRightRadius: 3,
     }
+
+
+
+
 };
 
 module.exports = MapPage;
